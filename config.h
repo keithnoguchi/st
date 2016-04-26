fb614b434949180fd053b6db277603c04ab27d54
Kei Nohguchi <kei@nohguchi.com>
2016-04-25 23:04:03 -0700

    config.h: Change the font size to 18 pixel.

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Liberation Mono:pixelsize=20:antialias=true:autohint=true:bold";
+static char font[] = "Liberation Mono:pixelsize=18:antialias=true:autohint=true:bold";
 static int borderpx = 2;
 
 /*
